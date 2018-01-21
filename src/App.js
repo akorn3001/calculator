f261f55d722b942589fd14aad0f0506ccb1fc63a
Alex Kornfeld <akorn3000@gmail.com>
2018-01-21 00:03:47 -0500

    Color code button types

--- src/App.js
+++ src/App.js
@@ -3,63 +3,52 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  state = {
+    displayValue: '0'
+  }
   
   render() {
+    const { displayValue } = this.state;
     return (
       <div className="App">
         <div className="calculator">
-          <div className="viewScreen"></div>
-          <button className="btn">⌫</button>
-          <div className="firstRow">
-            <button className="btn">C</button>
-            <button className="btn">( )</button>
-            <button className="btn">%</button>
-            <button className="btn">÷</button>
-          </div>
-          <div className="secondRow">
-            <button className="btn">7</button>
-            <button className="btn">8</button>
-            <button className="btn">9</button>
-            <button className="btn">×</button>
-          </div>
-          <div className="thirdRow">
-            <button className="btn">4</button>
-            <button className="btn">5</button>
-            <button className="btn">6</button>
-            <button className="btn">-</button>
-          </div>
-          <div className="fourthRow">
-            <button className="btn">1</button>
-            <button className="btn">2</button>
-            <button className="btn">3</button>
-            <button className="btn">+</button>
-          </div>
-          <div className="fifthRow">
-            <button className="btn">±</button>
-            <button className="btn">0</button>
-            <button className="btn">.</button>
-            <button className="btn">=</button>
+          <div className="viewScreen">{ displayValue }</div>
+          <div className="keypad">
+            <div className="firstRow">
+              <button className="btn btn-clear">C</button>
+              <button className="btn btn-parens">( )</button>
+              <button className="btn btn-percent">%</button>
+              <button className="btn btn-divide">÷</button>
+            </div>
+            <div className="secondRow">
+              <button className="btn btn-7">7</button>
+              <button className="btn btn-8">8</button>
+              <button className="btn btn-9">9</button>
+              <button className="btn btn-multiply">×</button>
+            </div>
+            <div className="thirdRow">
+              <button className="btn btn-4">4</button>
+              <button className="btn btn-5">5</button>
+              <button className="btn btn-6">6</button>
+              <button className="btn btn-subtract">-</button>
+            </div>
+            <div className="fourthRow">
+              <button className="btn btn-1">1</button>
+              <button className="btn btn-2">2</button>
+              <button className="btn btn-3">3</button>
+              <button className="btn btn-add">+</button>
+            </div>
+            <div className="fifthRow">
+              <button className="btn btn-negate">±</button>
+              <button className="btn btn-0">0</button>
+              <button className="btn btn-decimal">.</button>
+              <button className="btn btn-equals">=</button>
+            </div>
           </div>
         </div>
       </div>
     );
   }
 }
-
-// class App extends Component {
-//   render() {
-//     return (
-//       <div className="App">
-//         <header className="App-header">
-//           <img src={logo} className="App-logo" alt="logo" />
-//           <h1 className="App-title">Welcome to React</h1>
-//         </header>
-//         <p className="App-intro">
-//           To get started, edit <code>src/App.js</code> and save to reload.
-//         </p>
-//       </div>
-//     );
-//   }
-// }
 
 export default App;
